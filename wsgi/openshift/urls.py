2cce6f6c906c2c27ce348922d124723e7ef8f054
Ricardo M. de Oliveira <ricardo.martinelli.oliveira@gmail.com>
2015-03-24 14:00:11 -0300

    Corrigindo views.py

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'openshift.views.home', name='home'),
+    url(r'^/', 'openshift.views.home', name='home'),
     # url(r'^openshift/', include('openshift.foo.urls')),
     url(r'^dashboard/', 'openshift.views.home', name='dashboard'),
     # Uncomment the admin/doc line below to enable admin documentation:
