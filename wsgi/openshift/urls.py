dbb4baab75fb850d2ed5041034153f8d291acc2d
Ricardo M. de Oliveira <ricardo.martinelli.oliveira@gmail.com>
2015-03-25 23:27:04 -0300

    mudando views.py

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
     # url(r'^openshift/', include('openshift.foo.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^dashboard', 'openshift.views.dashboard'),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
