c00c7b59076ed8144f6d4cb9b7d58b8fd82eb4a6
Ricardo M. de Oliveira <ricardo.martinelli.oliveira@gmail.com>
2015-03-25 23:45:59 -0300

    mudando views.py

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -8,9 +8,6 @@ def home(request):
     return render_to_response('home/home.html')
 
 def dashboard(request):
-    try:
-        t = get_template('dashboard.html')
-            html = t.render()
-    except:
-        print sys_exec_info()[0]
+    t = get_template('dashboard.html')
+    html = t.render()
     return HttpResponse(html)
